6d090176d2c39144aa1bd5a1e498a90afa2b1341
tokorom <tokorom@gmail.com>
2014-01-18 23:37:42 +0900

    First commit

---
+++ CocoaPodsProjectSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CocoaPodsProjectSample
+//
+//  Created by ytokoro on 1/18/14.
+//  Copyright (c) 2014 tokoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CocoaPodsProjectSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CocoaPodsProjectSample
+//
+//  Created by ytokoro on 1/18/14.
+//  Copyright (c) 2014 tokoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
